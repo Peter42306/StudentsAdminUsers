3a94b21ff6dcf025abf3684bb66743bad6438b80
Petr Zalizko <pzalizko@gmail.com>
2025-04-13 17:45:02 +0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StudentsAdminUsers.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
